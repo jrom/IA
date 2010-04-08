4250e463c4de529b4982f4cde72ac2edd76c02b2
Bernat Farrero <dberni@gmail.com>
2010-04-08 20:02:51 +0200

    Solucio inicial 2 funcionant

--- IA/Main.java
+++ IA/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
   public static void main (String[] args)
   {
-    SquareBoard board = new SquareBoard();
+    SquareBoard board = new SquareBoard(10, 50);
     System.out.println(board);
     // TODO Loads of works
   }
--- IA/SquareBoard.java
+++ IA/SquareBoard.java
@@ -1,5 +1,5 @@
 package IA;
-
+import java.util.List;
 import java.util.Arrays;
 
 public class SquareBoard
@@ -7,32 +7,160 @@ public class SquareBoard
   // Globals and misc
   // Global N value
   public static int N = 20;
-
+	public static int K;
+	public static int P;
+	
   // Data
 
-  public SquareBoard()
-  {
+	Ruta[] rutes;
+	Parada[] parades;
 
+  public SquareBoard(int k, int p)
+  {
+		int i;
+		this.K = k;
+		this.P = p;
+		rutes = new Ruta[K];
+		parades = new Parada[P];
+		crearParades();
+		for (i = 0; i < K; i++)
+			rutes[i] = new Ruta();
+			
+		solIni2();
   }
 
+	private void crearParades()
+	{
+		int x,y, i;
+		for (i=0; i<P; i++)
+			parades[i] = new Parada(-1,-1,-1);
+
+		for (i=0; i<P; i++)
+		{
+			x = aleatori(N);
+			y = aleatori(N);
+			while (paradaOcupada(x,y))
+			{
+				x = aleatori(N);
+				y = aleatori(N);
+			}
+			parades[i] = new Parada(x,y, i);
+		}
+	}
+
+	public void solIni1()
+	{
+		
+	}
+
+	//dolent
+	public void solIni2()
+	{
+		int pi, ki;
+		ki = 0;
+		for (pi = 0; pi < P; pi++)
+		{
+			rutes[ki].afegirParada(pi);
+			ki = (ki + 1) % K;
+		}
+		
+	}
+	
+	private int aleatori(int max)
+	{
+		return new Double(Math.random() * max).intValue();		
+	}
+	
+	private boolean paradaOcupada(int x, int y)
+	{
+		if ((x == 0 && y == 0) || (x == 19 && y == 19)) return true;
+		for (int i = 0; i < P; i++)
+		{
+			if (parades[i].x == x && parades[i].y == y) return true;
+		}
+			
+
+		return false;
+	}
+	
+	private int idParada(int x, int y)
+	{
+		if (x == 0 && y == 0) return -10; // 0,0
+		else if (x == 19 && y == 19) return -20; // 19,19
+		for (int i = 0; i < P; i++)
+		{
+			if (parades[i].x == x && parades[i].y == y) return parades[i].id;
+		}
+		return -1; // No hi ha parada
+	}
+	
   public String toString()
   {
     String out = "";
     int i,j;
 
     out += "########################################\n";
-    out += "Soc un tauler de Squaretown!\n";
 
     for(i = 0; i < N; i++)
     {
       for(j = 0; j < N; j++)
       {
-        out += 0+" "; // TODO show the fucking matrix of something
+        if (paradaOcupada(i,j)) out +=" " + idParada(i,j) + " ";
+				else out += " - ";
       }
       out +="\n";
     }
     out += "########################################\n";
 
+		out += "Rutes:\n";
+		for (i = 0; i < K; i++)
+		{
+			out += "Ruta "+i+": ";
+			for(j = 0; j < rutes[i].numparades; j++)
+			{
+				out += rutes[i].parades[j]+" ";
+			}
+			out += "\n";
+		}
+
     return out;
   }
+
+
+	class Parada
+	{
+		public int id;
+		public int x;
+		public int y;
+
+		public Parada(int x_, int y_, int id_)
+		{
+			id = id_;
+			x = x_;
+			y = y_;
+		}
+	}
+
+	class Ruta
+	{
+		int[] parades;
+		int numparades;
+	// Suma de les distancies entre totes les parades seguint l'ordre de la ruta
+		int dist;
+
+		public Ruta()
+		{
+			numparades = 0;
+			dist = 0;
+			parades = new int[P];
+		}
+
+		public void afegirParada(int id_)
+		{
+			// TODO Comprovar que la parada no estigui ja a la ruta
+			parades[numparades] = id_;
+			numparades++;
+		}
+	}
 }
+
