0c8b0774600a48db67b1d5da428ae2ad4fb34177
Jordi Romero <jordi@jrom.net>
2010-04-09 12:58:36 +0200

    Finished GoalTest

--- IA/SquareGoalTest.java
+++ IA/SquareGoalTest.java
@@ -8,9 +8,7 @@ public class SquareGoalTest implements GoalTest
   public boolean isGoalState(Object state)
   {
 		SquareBoard board = (SquareBoard) state;
-    // TODO check if is a fucking goal state...
-
-    // Per ara retornem fals, segons l'enunciat en busqueda local no es pot saber si s'ha arribat a un estat final, per tant s'ha de negar sempre
+    // Retornem fals perque es tracta de cerca local
 		return false;
 	}
 
